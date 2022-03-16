50c9b0e93783c6a69287e7c4ee15548a0f911260
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-03-16 10:16:08 -0600

    Make Calculator a functional component
    modified:   src/components/calculator.js
    modified:   src/components/key.js
    modified:   src/components/keyRow.js

--- src/components/keyRow.js
+++ src/components/keyRow.js
@@ -2,25 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Key from './key';
 
-class KeyRow extends React.PureComponent {
-  render() {
-    const eleArray = [];
-    const { keys } = this.props;
-    for (let i = 0; i < keys.length; i += 1) {
-      let key = {};
-      if ('÷X-+='.includes(keys[i])) {
-        key = <Key name={keys[i]} key={keys[i]} className="orange" />;
-      } else key = <Key name={keys[i]} key={keys[i]} />;
-      eleArray.push(key);
-    }
-    const element = (
-      <div className="keyRow">
-        {eleArray}
-      </div>
-    );
-    return element;
+const KeyRow = (props) => {
+  const eleArray = [];
+  const { keys } = props;
+  for (let i = 0; i < keys.length; i += 1) {
+    let key = {};
+    if ('÷X-+='.includes(keys[i])) {
+      key = <Key name={keys[i]} key={keys[i]} className="orange" />;
+    } else key = <Key name={keys[i]} key={keys[i]} />;
+    eleArray.push(key);
   }
-}
+  return (
+    <div className="keyRow">
+      {eleArray}
+    </div>
+  );
+};
 
 KeyRow.propTypes = {
   keys: PropTypes.arrayOf(PropTypes.string).isRequired,
